6ef838c98112ebbc112cc710a568dab998a45f4f
kmj <meigieq@naver.com>
2025-10-15 06:31:12 +0900

    Update opensearch_client.py

--- OpenSearch/utils/opensearch_client.py
+++ OpenSearch/utils/opensearch_client.py
@@ -2,10 +2,11 @@ from opensearchpy import OpenSearch
 
 def get_client():
     return OpenSearch(
-        hosts=[{"host": "43.202.20.141", "port": 9201}], # AWS OpenSearch
+        hosts=[{"host": "43.200.220.6", "port": 9201}], # AWS OpenSearch
         http_auth=("admin","MYPassword1234!"),
         use_ssl=True,
         verify_certs=False,
         ssl_assert_hostname=False,
         ssl_show_warn=False
     )
+
